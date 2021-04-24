df1752a3163c23999b54c213a0217b3bd60a4d2f
Angelica Landazabal <angelica.informatik@gmail.com>
2021-04-24 15:29:21 -0500

    Creating models in myapi

---
+++ pruebaMonoku/myapi/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+    
+class Band(models.Model):
+    Name = models.CharField(max_length=200, null=False)
+
+class Artist(models.Model):
+    Name = models.CharField(max_length=200, null=False)
+    band = models.ForeignKey(Band, on_delete=models.CASCADE)
+
+class Album(models.Model):
+    title = models.CharField(max_length=300, null=False)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+class Genre(models.Model):
+    description = models.CharField(max_length=200, null=False)
+
+class Subgenre(models.Model):
+    description = models.CharField(max_length=200, null=False)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+class Tag(models.Model):
+    description = models.CharField(max_length=200, null=False)
+
+class Instrument(models.Model):
+    description = models.CharField(max_length=200, null=False)
+
+class Song(models.Model):
+
+    title = models.CharField(
+        max_length=256,
+        null=False
+    )
+
+    duration = models.CharField(
+        max_length=256,
+        null=False
+    )
+
+    date = models.DateTimeField(null=False)
+
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+
+    subgenres = models.ManyToManyField(Subgenre)
+
+    tags = models.ManyToManyField(Tag)
+
+    instruments = models.ManyToManyField(Instrument)
+
+    def __str__(self):
+        return '{} album {} date {}'.format(self.title, self.album, self.date)
+
--- pruebaMonoku/pruebaMonoku/settings.py
+++ pruebaMonoku/pruebaMonoku/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapi.apps.MyapiConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'pruebaMonoku.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'prueba_monoku',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
     }
 }
 
