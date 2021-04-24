5ffb0f92e64e4c1c19845be234a169c85ca80e64
Angelica Landazabal <angelica.informatik@gmail.com>
2021-04-24 15:40:27 -0500

    Register models with the admin site

--- pruebaMonoku/myapi/admin.py
+++ pruebaMonoku/myapi/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Song, Album, Artist, Band, Genre, Instrument, Subgenre, Tag
+
+admin.site.register(Song)
+admin.site.register(Album)
+admin.site.register(Artist)
+admin.site.register(Band)
+admin.site.register(Genre)
+admin.site.register(Instrument)
+admin.site.register(Subgenre)
+admin.site.register(Tag)
